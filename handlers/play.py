da25da0a4bc5aa864acf46a74e3bfd00fa89d3aa
UNREALBOT12 <91039807+UNREALBOT12@users.noreply.github.com>
2021-10-19 10:08:11 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -193,10 +193,10 @@ async def play(_, message: Message):
                     [
                         InlineKeyboardButton(
                             text="𝐂𝐡𝐚𝐧𝐧𝐞𝐥📡",
-                            url=f"https://t.me/eSport_BOTs"),
+                            url=f"https://t.me/RAW_X_MUSIC_OFFICIAL"),
                         InlineKeyboardButton(
                             text="𝐆𝐫𝐨𝐮𝐩⭐",
-                            url=f"https://t.me/EsportClan")
+                            url=f"https://t.me/we_are_legendss")
 
                     ]
                 ]
